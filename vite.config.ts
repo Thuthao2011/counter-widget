368bf624f41cf569649ce7d40fc7a04f02125710
vietthang09 <vthcvn@gmail.com>
2025-07-28 14:20:02 +0700

    Refactor HTML structure and update React version in index.html; enhance Vite config for output directory and asset management

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist',
+    emptyOutDir: true,
     lib: {
       entry: 'src/index.tsx',
       name: 'MyWidget',
       fileName: () => `widget.js`,
-      formats: ['iife'] 
+      formats: ['iife']
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
@@ -16,7 +18,8 @@ export default defineConfig({
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM'
-        }
+        },
+        assetFileNames: 'counter-widget.css',
       }
     }
   }
