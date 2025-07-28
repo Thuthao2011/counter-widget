af563a8731d3703ebf505a50d9c7d510cd2b6461
vietthang09 <vthcvn@gmail.com>
2025-07-28 14:45:44 +0700

    reinit

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    lib: {
-      entry: 'src/index.tsx',
-      name: 'MyWidget',
-      fileName: () => `widget.js`,
-      formats: ['iife']
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
-        },
-        assetFileNames: 'counter-widget.css',
-      }
-    }
-  }
 })
